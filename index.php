f086fc85c44c5b54241645fa645f9b20709a606d
pranjitbiswas72 <pranjitbiswas205@gmail.com>
2025-10-08 20:52:09 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,71 +1,61 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php_homework</title>
+    <title>file upload</title>
 </head>
+
 <body>
+
+
+    <!--file input form -->
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <input type="submit" name="submit" value="upload">
+    </form>
+
     <?php
-    echo "Hello world,print <br>"; //echo youse 
-    print "Hello world,print <br>"; // print youse
-    printf("Hello world ,print <br>");//printf youse
-    print_r("Hello world,print <br>");// print_r youse
+
+    if (isset($_POST['submit'])) {
+         $file =$_FILES['file'];
+
+        $file_name = $_FILES['file']['name'];
+        $temp_name = $_FILES['file']['tmp_name'];
+        $file_size = $_FILES['file']['size'];
+        $fileError =$_FILES['file']['error'];
+        $fileType = $_FILES['file']['type'];
+
+        $fileExt = explode('.', $file_name);
+        $fileActualExt = strtolower(end($fileExt));
+        
+        $allowed =array('jpg', 'png', 'jpeg', 'pdf');
 
 
-    // varibole declare
-
-    $name ="pranjit"; //string
-    $age =21;         //number
-    var_dump($age);   //bolean
-
-
-    // constant declare
-    // clase constant
-    define("SITE_NAME", "I'come to home <br>");
-     echo SITE_NAME;
-     
-     // constant array
-     define("cars",[
-     "pranjit",
-     "BMW",
-     "toyota<br>"]);
-
-     echo cars[2];
-
-     // Global varibole use
-
-      $name="pranjit  </br>";
-      
-    define("POST_NAME", "mywebsite<br>");
-        function greet(){
-        echo POST_NAME.$GLOBALS['name'];
+        if (in_array($fileActualExt, $allowed)) {
+            if ($fileError ===0){
+                if ($file_size <1000000){
+                $fileNameNew =uniqid('', true).".".$
+                $fileActualExt;
+                $fileDestination = 'uploads/'.$fileNameNew;
+               $fileNameNew = uniqid('', true) . "." . $fileActualExt;
+                header("Location: index.php?uploadsuccess");
+            }else{
+            echo "Your file is too big!";
+            }
+        } else{
+            echo "There was an error uploading your file!";
         }
-       greet();
-     // string-int
+       
+        }
+    }
 
-       $x=30;
-       $y=(int)$x;
-
-       echo$y . "<br>";
-       echo gettype($y). "<br>"; 
-
-     //int-float
-     $x =100;
-     $y =3.14;
-     echo is_float($y). "<br>";
-     echo gettype($y). "<br>";
-    
-    
-
-
-
-
-
-
-    
-    
     ?>
-    
+
+
+
 </body>
+
 </html>
